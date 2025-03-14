a060bd8618d223cc31159724e18b520bce1d494b
Wilbur W <wilburwei.2008@gmail.com>
2025-03-13 23:19:17 -0400

    Add navigation link functionality

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ window.onload = function () {
   $("#navbar h1").on("click",  function() {
     window.location.href="/"
   })
+  $("#navlinks p").on("click", function() {
+    let ival = $(this).html();
+    switch (ival) {
+      case "About Us":
+        window.location.href="/about.html"
+        break;
+      case "Our Blog":
+        window.location.href="/blog.html"
+        break;
+      case "Get a Quote":
+        window.location.href="/quote.html"
+        break;
+    }
+  
+  })
 }
